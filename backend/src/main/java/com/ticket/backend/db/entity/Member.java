400ed1866dac6d31e8f09e26513c716136faa8e9
DESKTOP-6OO0P6F\SSAFY <sonhm48021@gmail.com>
2026-02-23 12:06:58 +0900

    feat: 회원 엔티티 추가 및 권한 Enum 추가

--- backend/src/main/java/com/ticket/backend/db/entity/Member.java
+++ backend/src/main/java/com/ticket/backend/db/entity/Member.java
@@ -1,4 +1,37 @@
 package com.ticket.backend.db.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique=true)
+    private String username;
+
+    @Column
+    private String password;
+
+    @Column
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    /*@OneToMany(mappedBy = "member", cascade = CascadeType.ALL)
+    private List<Ticketing> ticketList;*/
+
+    public void changePW(String password) {
+        this.password = password;
+    }
+
 }
---
+++ backend/src/main/java/com/ticket/backend/db/entity/Role.java
@@ -0,0 +1,5 @@
+package com.ticket.backend.db.entity;
+
+public enum Role {
+    ADMIN, USER
+}
